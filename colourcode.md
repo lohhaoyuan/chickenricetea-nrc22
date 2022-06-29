33f6ec3cdd408a1ea536fe5ee95e3f08fa37446e
hyloh <you@example.com>
2022-06-29 18:17:12 +0800

    fixed wavy line thing

--- colourcode.md
+++
@@ -1,5 +0,0 @@
-## COLOUR CODES FOR EV3 (professional storage)
-pls store them here
-
-# Threshold for P-tracking
-31.5
--- nrc/main.py
+++ nrc/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -7,10 +6,6 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import os, sys
-
-# This program requires LEGO EV3 MicxroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
 
 
 # Create your objects here.
@@ -52,4 +47,3 @@ def PID(target, actual, kp, ki, kd):
 
 
 # the savepeopleandpickstuffup algorithm
-
