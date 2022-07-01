5fd1462fdfaca4aedd70e004335f3b7fc146ae34
hyloh <you@example.com>
2022-07-01 17:33:22 +0800

    commit

--- nrc/main.py
+++ nrc/main.py
@@ -8,6 +8,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 from math import *
+from statistics import *
 # Create your objects here.
 ev3 = EV3Brick()
 MotorLeft = Motor(Port.A)
@@ -28,6 +29,8 @@ def PID_Gyro(threshold, target, actual):
     integral = 0
     derivative   = 0
     output = 0
+    error = target - actual
+    previous = error
     while abs(error) > threshold:
         error = target - actual
         integral += error
@@ -37,9 +40,21 @@ def PID_Gyro(threshold, target, actual):
         MotorRight.run(output * -1)
         previous = error
         print(f"error is now {error}, output is now: {output}")
-def ITWONTKILLYOUYUZHE(threshold_that_didnt_kill_yuzhe, target_that_didnt_kill_yuzhe, actual_that_might_have_killed_yuzhe):
-    pass
-# actual program
+        
+def PID_Colour(threshold, left_rgb, right_rgb, actual_rgb): # look yuzhe you arent dead
+    kp = 1
+    ki = 1
+    kd = 1
+    integral = 0 
+    derivative = 0
+    output = 0
+    error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
+    previous = error
+    while abs(mean(error)) > threshold:
+        error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
+        integral += mean(error)
+        derivative = (error(1)-previous(1),error(2)-previous(2),error(0)-previous(0))
+# actual prrogram
 # Testing
 PID_Gyro(2,0,gyro_sensor.angle())
 # the picking stuff up FUNCTION
