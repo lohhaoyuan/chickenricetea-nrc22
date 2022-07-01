d13db4d04c5b1e529590291a851601b0eaa0a866
hyloh <you@example.com>
2022-07-01 18:05:21 +0800

    commit

--- nrc/main.py
+++ nrc/main.py
@@ -37,23 +37,22 @@ def PID_Gyro(threshold, target, actual):
         derivative = error - previous
         output = (kp * error) + (ki * integral) + (kd * derivative)
         MotorLeft.run(output)
-        MotorRight.run(output * -1)
-        previous = error
+        MotorRight.run(output * -1) 
         print(f"error is now {error}, output is now: {output}")
         
-def PID_Colour(threshold, left_rgb, right_rgb, actual_rgb): # look yuzhe you arent dead
-    kp = 1
-    ki = 1
-    kd = 1
-    integral = 0 
-    derivative = 0
-    output = 0
-    error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
-    previous = error
-    while abs(mean(error)) > threshold:
-        error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
-        integral += mean(error)
-        derivative = (error(1)-previous(1),error(2)-previous(2),error(0)-previous(0))
+# def PID_Colour(threshold, left_rgb, right_rgb, actual_rgb): # look yuzhe you arent dead
+#     kp = 1
+#     ki = 1
+#     kd = 1
+#     integral = 0 
+#     derivative = 0
+#     output = 0
+#     error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
+#     previous = error
+#     while abs(mean(error)) > threshold:
+#         error = (target_rgb(1)-actual_rgb(1),target_rgb(2)-actual_rgb(2),target_rgb(0)-actual_rgb(0))
+#         integral += mean(error)
+#         derivative = (error(1)-previous(1),error(2)-previous(2),error(0)-previous(0))
 # actual prrogram
 # Testing
 PID_Gyro(2,0,gyro_sensor.angle())
